58346a2ba252594a276585987e842fea66e4c641
Thiago Coelho Medeiros <88511641+kposk@users.noreply.github.com>
2023-02-27 14:27:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from detector import *
+import os
+import cv2
 
 detec = Detector()
 
-detec.onImage("5.png")
 
-print(detec.cfg)
+print("Choose the file you want to detect:\n")
+print(f'1: Video\n'
+       '2: Image\n')
+print("Type here: ", end="")
+choice = int(input())
+
+
+if choice == 1:
+    print("VIDEO FULL PATH: ", end="")
+    videoPath = input()
+    #'/home/v3d/Downloads/ovelha420.mp4'
+
+    detec.onVideo(videoPath)
+
+elif choice == 2:
+
+    print("IMAGE FULL PATH: ", end="")
+    imagePath = input()
+    
+    detec.onImage(imagePath)
+    #"/home/v3d/Documents/detectron2/4.png"
+else:
+    exit(1)
+
+
+
